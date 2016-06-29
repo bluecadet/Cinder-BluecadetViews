38e676dbf9ce8612500c9d2d7d65d1fba29de8b8
swiley <stacey@bluecadet.com>
2016-06-29 15:00:37 -0600

    updating touchView to work with touch block format

--- src/TouchView.cpp
+++ src/TouchView.cpp
@@ -1,6 +1,7 @@
+
+#include "TouchView.h"
+
 #include "cinder/gl/gl.h"
-#include "TouchManager.h"
-#include "BaseTouchView.h"
 #include "boost/lexical_cast.hpp"
 #include <math.h>
 
@@ -8,203 +9,208 @@ using namespace std;
 using namespace ci;
 using namespace ci::app;
 
-namespace touch {
+namespace bluecadet {
+namespace views {
 
-	int	BaseTouchView::TotalObjectCount = 0;
-	int	BaseTouchView::ObjectID = 0;
+int	TouchView::TotalObjectCount = 0;
+int	TouchView::ObjectID = 0;
 
-	//==================================================
-	// Setup/Destruction
-	//
+//==================================================
+// Setup/Destruction
+//
 
-	BaseTouchView::BaseTouchView(bool dragEnabled) :
-		BaseView(),
-		mSize(0.0f),
-		mUniqueID(ObjectID),
-		mDragEnabled(dragEnabled),
-		mTouchEnabled(true),
-		mMultiTouchEnabled(false),
-		mHasMovingTouches(false),
-		mAllowsTapReleaseOutside(false),
-		mIsDragging(false),
-		mIsActive(false),
-		mDragThreshold(20.0f),
-		mMaxTapDuration(2.0),
-		mCurTouchPos(0, 0),
-		mPrevTouchPos(0, 0),
-		mInitialRelTouchPos(0, 0),
-		mInitialAbsTouchPos(0, 0),
-		mInitialPosWhenTouched(0, 0),
-		mInitialTouchTime(0)
-	{
-		TotalObjectCount++;
-		ObjectID++;
+TouchView::TouchView(bool dragEnabled) :
+	BaseView(),
+	mSize(0.0f),
+	mUniqueID(ObjectID),
+	mDragEnabled(dragEnabled),
+	mTouchEnabled(true),
+	mMultiTouchEnabled(false),
+	mHasMovingTouches(false),
+	mAllowsTapReleaseOutside(false),
+	mIsDragging(false),
+	mIsActive(false),
+	mDragThreshold(20.0f),
+	mMaxTapDuration(2.0),
+	mCurTouchPos(0, 0),
+	mPrevTouchPos(0, 0),
+	mInitialRelTouchPos(0, 0),
+	mInitialAbsTouchPos(0, 0),
+	mInitialPosWhenTouched(0, 0),
+	mInitialTouchTime(0)
+{
+	TotalObjectCount++;
+	ObjectID++;
+}
+
+TouchView::~TouchView() {
+	// Call to unregister should be made BEFORE you destroy your touchable object
+	mPath.clear();
+	TotalObjectCount--;
+}
+
+void TouchView::reset() {
+	BaseView::reset();
+	cancelTouches();
+}
+
+void TouchView::setup(const cinder::vec2 &size) {
+	// Create shape from this position and size
+	vector<cinder::vec2> coordinates;
+	vec2 startCoord = vec2(0);
+	coordinates.push_back(startCoord);
+	coordinates.push_back(vec2(startCoord.x + size.x, startCoord.y));
+	coordinates.push_back(vec2(startCoord.x + size.x, startCoord.y + size.y));
+	coordinates.push_back(vec2(startCoord.x, startCoord.y + size.y));
+	createShape(coordinates);
+	// Clean up
+	coordinates.clear();
+}
+
+void TouchView::setup(float radius) {
+
+	// Touchable area of each circle is created from 50 points. This number may be adjusted.
+	int points = 30;
+	double slice = 2 * M_PI / points;
+	vector<cinder::vec2> coordinates;
+	for (int i = 0; i < points; i++) {
+		double angle = slice * i;
+		int newX = (int)(radius *  cos(angle));
+		int newY = (int)(radius * sin(angle));
+		coordinates.push_back(vec2(newX, newY));
 	}
 
-	BaseTouchView::~BaseTouchView() {
-		//The call to unregister should be made BEFORE you destroy your touchable object
-		mPath.clear();
-		TotalObjectCount--;
+	createShape(coordinates);
+	coordinates.clear();
+}
+
+void TouchView::setup(const std::vector<cinder::vec2> &coordinates, const cinder::vec2 &pos) {
+	createShape(coordinates);
+}
+
+void TouchView::createShape(const std::vector<cinder::vec2> &coordinates) {
+	mPath = cinder::Path2d();
+
+	for (int i = 0; i <= coordinates.size() - 1; i++) {
+		if (i == 0) mPath.moveTo(coordinates[0]);
+		else mPath.lineTo(coordinates[i]);
 	}
 
-	void BaseTouchView::reset() {
-		BaseView::reset();
-		cancelTouches();
+	mPath.close();
+
+	mSize = mPath.calcBoundingBox().getSize();
+}
+
+//==================================================
+// Touch Management
+//
+
+void TouchView::touchesBeganHandler(int touchID, const ci::vec2 &touchPnt, TouchType touchType) {
+	mObjectTouchIDs.push_back(touchID);
+
+	mPrevTouchPos = touchPnt; // Set to current touchPnt, otherwise prevtouch pos may be anywhere
+	mCurTouchPos = touchPnt;
+	mInitialRelTouchPos = touchPnt;
+	mInitialAbsTouchPos = convertLocalToGlobal(touchPnt);
+	mInitialPosWhenTouched = getGlobalPosition();
+	mInitialTouchTime = getElapsedSeconds();
+	mIsDragging = getNumTouches() == 1 ? false : mIsDragging;
+	mHasMovingTouches = getNumTouches() == 1 ? false : mHasMovingTouches;
+
+	mDidBeginTouch(shared_from_this());
+}
+
+void TouchView::touchesMovedHandler(int touchID, const ci::vec2 &touchPnt, TouchType touchType) {
+	if (mObjectTouchIDs.empty() || mObjectTouchIDs.front() != touchID) {
+		return;
 	}
 
-	void BaseTouchView::setupBaseTouchObj(const cinder::vec2 &size, bool registerWithTouchManager) {
-		//create shape from this position and size
-		vector<cinder::vec2> coordinates;
-		vec2 startCoord = vec2(0);
-		coordinates.push_back(startCoord);
-		coordinates.push_back(vec2(startCoord.x + size.x, startCoord.y));
-		coordinates.push_back(vec2(startCoord.x + size.x, startCoord.y + size.y));
-		coordinates.push_back(vec2(startCoord.x, startCoord.y + size.y));
-		createShape(coordinates);
-		//clean up
-		coordinates.clear();
+	mHasMovingTouches = true;
+
+	if (!mDragEnabled) {
+		return;
 	}
 
-	void BaseTouchView::setupBaseTouchObj(float radius, bool registerWithTouchManager) {
-		//! Touchable area of each circle is created from 50 points. This number may be adjusted.
-		int points = 30;
-		double slice = 2 * M_PI / points;
-		vector<cinder::vec2> coordinates;
-		for (int i = 0; i < points; i++) {
-			double angle = slice * i;
-			int newX = (int)(radius *  cos(angle));
-			int newY = (int)(radius * sin(angle));
-			coordinates.push_back(vec2(newX, newY));
-		}
+	mPrevTouchPos = mCurTouchPos;
+	mCurTouchPos = touchPnt;
 
-		createShape(coordinates);
-		coordinates.clear();
+	if (!mIsDragging) {
+		const float dragDistance2 = glm::distance2(mInitialAbsTouchPos, convertLocalToGlobal(touchPnt));
+		mIsDragging = dragDistance2 > mDragThreshold * mDragThreshold;
 	}
 
-	void BaseTouchView::setupBaseTouchObj(const std::vector<cinder::vec2> &coordinates, const cinder::vec2 &pos, bool registerWithTouchManager) {
-		createShape(coordinates);
+	mDidMoveTouch(shared_from_this());
+}
+
+void TouchView::touchesEndedHandler(int touchID, const ci::vec2 &touchPnt, TouchType touchType) {
+	mDidEndTouch(shared_from_this());
+
+	bool didTap = mAllowsTapReleaseOutside || hasTouchPoint(touchPnt);
+
+	// Only allow taps within a certain time
+	if (didTap) {
+		double touchDuration = getElapsedSeconds() - mInitialTouchTime;
+		didTap = touchDuration <= mMaxTapDuration;
 	}
 
-	void BaseTouchView::createShape(const std::vector<cinder::vec2> &coordinates) {
-		mPath = cinder::Path2d();
-
-		for (int i = 0; i <= coordinates.size() - 1; i++) {
-			if (i == 0) mPath.moveTo(coordinates[0]);
-			else mPath.lineTo(coordinates[i]);
-		}
-
-		mPath.close();
-
-		mSize = mPath.calcBoundingBox().getSize();
+	// Only allow taps within a certain drag distance
+	if (didTap && mIsDragging) {
+		didTap = false;
 	}
 
-	//==================================================
-	// Touch Management
-	//
-
-	void BaseTouchView::touchesBeganHandler(int touchID, const ci::vec2 &touchPnt, touch::TouchType touchType) {
-		mObjectTouchIDs.push_back(touchID);
-
-		mPrevTouchPos = touchPnt; // Set to current touch pt, otherwise prevtouch pos may be anywhere
-		mCurTouchPos = touchPnt;
-		mInitialRelTouchPos = touchPnt;
-		mInitialAbsTouchPos = convertLocalToGlobal(touchPnt);
-		mInitialPosWhenTouched = getGlobalPosition();
-		mInitialTouchTime = getElapsedSeconds();
-		mIsDragging = getNumTouches() == 1 ? false : mIsDragging;
-		mHasMovingTouches = getNumTouches() == 1 ? false : mHasMovingTouches;
-
-		mDidBeginTouch(shared_from_this());
+	// Trigger tap if we had one
+	if (didTap) {
+		mDidTap(shared_from_this());
 	}
 
-	void BaseTouchView::touchesMovedHandler(int touchID, const ci::vec2 &touchPnt, touch::TouchType touchType) {
-		if (mObjectTouchIDs.empty() || mObjectTouchIDs.front() != touchID) {
-			return;
-		}
+	resetTouchState();
+}
 
-		mHasMovingTouches = true;
+void TouchView::cancelTouches() {
+	/*
+	std::shared_ptr<TouchManager> manager = TouchManager::getInstance();
 
-		if (!mDragEnabled) {
-			return;
-		}
+	const auto sharedPtr = shared_from_this();
+	const auto touchIds = vector<int>(mObjectTouchIDs.begin(), mObjectTouchIDs.end());
 
-		mPrevTouchPos = mCurTouchPos;
-		mCurTouchPos = touchPnt;
-
-		if (!mIsDragging) {
-			const float dragDistance2 = glm::distance2(mInitialAbsTouchPos, convertLocalToGlobal(touchPnt));
-			mIsDragging = dragDistance2 > mDragThreshold * mDragThreshold;
-		}
-
-		mDidMoveTouch(shared_from_this());
+	for (const auto i : touchIds) {
+		manager->endTouch(i);
+		mDidCancelTouch(sharedPtr);
 	}
 
-	void BaseTouchView::touchesEndedHandler(int touchID, const ci::vec2 &touchPnt, touch::TouchType touchType) {
-		mDidEndTouch(shared_from_this());
+	mObjectTouchIDs.clear();
+	*/
+}
 
-		bool didTap = mAllowsTapReleaseOutside || hasTouchPoint(touchPnt);
+void TouchView::resetTouchState() {
+	mIsDragging = false;
+	mHasMovingTouches = false;
 
-		// only allow taps within a certain time
-		if (didTap) {
-			double touchDuration = getElapsedSeconds() - mInitialTouchTime;
-			didTap = touchDuration <= mMaxTapDuration;
-		}
+	mInitialRelTouchPos = vec2(0.0f, 0.0f);
+	mInitialAbsTouchPos = vec2(0.0f, 0.0f);
+	mInitialPosWhenTouched = vec2(0.0f, 0.0f);
+	mInitialTouchTime = 0.0;
 
-		// only allow taps within a certain drag distance
-		if (didTap && mIsDragging) {
-			didTap = false;
-		}
+	mObjectTouchIDs.clear();
+}
 
-		// trigger tap if we had one
-		if (didTap) {
-			mDidTap(shared_from_this());
-		}
+bool TouchView::hasTouchPoint(const vec2 &pnt) {
+	vec2 localPoint = convertGlobalToLocal(pnt);
+	return mPath.contains(localPoint);
+}
 
-		resetTouchState();
-	}
+bool TouchView::canAcceptTouch() const {
+	return mMultiTouchEnabled || mObjectTouchIDs.empty();
+}
 
-	void BaseTouchView::cancelTouches() {
-		std::shared_ptr<TouchManager> manager = TouchManager::getInstance();
+//==================================================
+// Debugging
+//
 
-		const auto sharedPtr = shared_from_this();
-		const auto touchIds = vector<int>(mObjectTouchIDs.begin(), mObjectTouchIDs.end());
+void TouchView::drawDebugShape() const {
+	gl::lineWidth(2.0f);
+	gl::draw(mPath);
+}
 
-		for (const auto i : touchIds) {
-			manager->endTouch(i);
-			mDidCancelTouch(sharedPtr);
-		}
-
-		mObjectTouchIDs.clear();
-	}
-
-	void BaseTouchView::resetTouchState() {
-		mIsDragging = false;
-		mHasMovingTouches = false;
-
-		mInitialRelTouchPos = vec2(0.0f, 0.0f);
-		mInitialAbsTouchPos = vec2(0.0f, 0.0f);
-		mInitialPosWhenTouched = vec2(0.0f, 0.0f);
-		mInitialTouchTime = 0.0;
-
-		mObjectTouchIDs.clear();
-	};
-
-	bool BaseTouchView::hasTouchPoint(const vec2 &pnt) {
-		vec2 localPoint = convertGlobalToLocal(pnt);
-		return mPath.contains(localPoint);
-	}
-
-	bool BaseTouchView::canAcceptTouch() const {
-		return mMultiTouchEnabled || mObjectTouchIDs.empty();
-	}
-
-	//==================================================
-	// Debugging
-	//
-
-	void BaseTouchView::drawDebugShape() const {
-		gl::lineWidth(2.0f);
-		gl::draw(mPath);
-	}
-
-};
+}
+}
