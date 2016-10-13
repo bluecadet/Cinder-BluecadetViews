c280a704a78604e1124af86f26bba344e879bb2f
swiley <stacey@bluecadet.com>
2016-10-13 15:09:48 -0600

    removing testing function

--- src/TextView.h
+++ src/TextView.h
@@ -66,7 +66,6 @@ protected:
 
 	ci::gl::Texture::Format createTextureFormat(bool smoothScaling) const;
 	inline void		invalidate(const bool layout = true, const bool size = true) override;
-	inline void		invalidate(const bool transforms = true, const bool content = true) override;
 
 	// Change visibility of these methods from public to protected since setSize()/getSize() should be used.
 	virtual ci::vec2		getMaxSize() const override { return StyledTextLayout::getMaxSize(); };
