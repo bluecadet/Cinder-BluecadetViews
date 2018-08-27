6503d412c63178701885efb5350d29843451589e
adielfernandez <adiel@bluecadet.com>
2018-08-27 17:38:56 -0400

    Add support to StrokedRoundedRectView for individual corner control

--- src/bluecadet/views/StrokedRoundedRectView.h
+++ src/bluecadet/views/StrokedRoundedRectView.h
@@ -37,17 +37,36 @@ public:
 	//! Cancels StrokedRoundedRectView animations in addition to BaseView animations.
 	void cancelAnimations() override;
 
+	//! Set view to accept different radii for each corner
+	inline void			useUniformCorners(bool useUniform) { mUniformCorners = useUniform; }
+
+	inline ci::Anim<float> &		getTopLeftCornerRadius() { return mTopLeftRadius; }
+	inline ci::Anim<float> &		getTopRightCornerRadius() { return mTopRightRadius; }
+	inline ci::Anim<float> &		getBottomRightCornerRadius() { return mBottomRightRadius; }
+	inline ci::Anim<float> &		getBottomLeftCornerRadius() { return mBottomLeftRadius; }
+	inline void						setTopLeftCornerRadius(const float value) { mTopLeftRadius = value; invalidate(false, true); }
+	inline void						setTopRightCornerRadius(const float value) { mTopRightRadius = value; invalidate(false, true); }
+	inline void						setBottomRightCornerRadius(const float value) { mBottomRightRadius = value; invalidate(false, true); }
+	inline void						setBottomLeftCornerRadius(const float value) { mBottomLeftRadius = value; invalidate(false, true); }
+	void							setCornerRadii(const float topLeft, const float topRight, const float bottomRight, const float bottomLeft);
+
+
 protected:
 	void draw() override;
 
 	static ci::gl::BatchRef		getSharedBatch();
 	static ci::gl::GlslProgRef	getSharedProg();
 
-	ci::Anim<ci::ColorA> mStrokeColor = ci::ColorA::white();
-	ci::Anim<float> mSmoothness = 1.0f;
-	ci::Anim<float> mStrokeWidth = 1.0f;
-	ci::Anim<float> mCornerRadius = 0.0f;
+	ci::Anim<ci::ColorA>		mStrokeColor = ci::ColorA::white();
+	ci::Anim<float>				mSmoothness = 1.0f;
+	ci::Anim<float>				mStrokeWidth = 1.0f;
+	ci::Anim<float>				mCornerRadius = 0.0f;
 
+	bool						mUniformCorners = true;
+	ci::Anim<float>				mTopLeftRadius = 0.0f;
+	ci::Anim<float>				mTopRightRadius = 0.0f;
+	ci::Anim<float>				mBottomRightRadius = 0.0f;
+	ci::Anim<float>				mBottomLeftRadius = 0.0f;
 };
 
 }
