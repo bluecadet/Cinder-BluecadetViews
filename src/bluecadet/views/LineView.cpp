74788de8816fa001a33616b9a3b3626889ec3d0b
adielfernandez <adiel@bluecadet.com>
2018-11-30 13:50:31 -0500

    Add screen camera scaling to line view

--- src/bluecadet/views/LineView.cpp
+++ src/bluecadet/views/LineView.cpp
@@ -26,7 +26,8 @@ void LineView::draw() {
 	//! This was introduced because of a bug that was causing gl::ScopedColor to override the parent views tint/alpha
 	ColorA correctedColor = mLineColor.value() * getDrawColor();
 	gl::ScopedColor color(correctedColor);
-	gl::ScopedLineWidth lineWidth(mLineWidth);
+	float screenScale = bluecadet::core::ScreenCamera::getInstance()->getScale().x;
+	gl::ScopedLineWidth lineWidth(mLineWidth * screenScale);
 	gl::drawLine(vec2(0, 0), getEndPoint());
 }
 
